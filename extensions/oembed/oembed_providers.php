c55e450ed75cf2e36e7111fc82a2635bc7c7b891
pivotlog <pivotlog@a2bfabd2-e71a-0410-9f27-bcdfd5ae8e08>
2010-05-30 10:30:56 +0000

    Added oEmbed toe extensions/

---
+++ extensions/oembed/oembed_providers.php
@@ -0,0 +1,56 @@
+<?php
+// default provider
+$providers['default'] = array('endpoint' => 'http://oohembed.com/oohembed/', 'format' => 'json', 'type' => 'rich');
+
+// specific providers by url pattern
+$providers['nos.nl'] = array(
+	'endpoint' => 'http://nos.nl/service/oembed/',
+	'format' => 'json',
+	'type' => 'video');
+$providers['youtube.com'] = array(
+	'endpoint' => 'http://www.youtube.com/oembed',
+	'format' => 'json',
+	'type' => 'video');
+$providers['flickr.com'] = array(
+	'endpoint' => 'http://www.flickr.com/services/oembed/',
+	'format' => 'json',
+	'type' => 'photo');
+$providers['viddler.com'] = array(
+	'endpoint' => 'http://lab.viddler.com/services/oembed/',
+	'format' => 'json',
+	'type' => 'video');
+$providers['qik.com'] = array(
+	'endpoint' => 'http://qik.com/api/oembed.json',
+	'format' => false,
+	'type' => 'video');
+$providers['revision3.com'] = array(
+	'endpoint' => 'http://revision3.com/api/oembed/',
+	'format' => 'json',
+	'type' => 'video');
+$providers['hulu.com'] = array(
+	'endpoint' => 'http://www.hulu.com/api/oembed.json',
+	'format' => false,
+	'type' => 'video');
+$providers['vimeo.com'] = array(
+	'endpoint' => 'http://www.vimeo.com/api/oembed.json',
+	'format' => false,
+	'type' => 'video');
+$providers['polleverywhere.com'] = array(
+	'endpoint' => 'http://www.polleverywhere.com/services/oembed/',
+	'format' => 'json',
+	'type' => 'rich');
+$providers['my.opera.com'] = array(
+	'endpoint' => 'http://my.opera.com/service/oembed',
+	'format' => 'json',
+	'type' => 'rich');
+$providers['5min.com'] = array(
+	'endpoint' => 'http://api.5min.com/oembed.json',
+	'format' => false,
+	'type' => 'video');
+$providers['blip.tv'] = array(
+	'endpoint' => 'http://blip.tv/oembed/',
+	'format' => 'json',
+	'type' => 'video');
+//$providers[''] = array('endpoint' => '', 'format' => 'json', 'type' => 'video');
+
+?>
---
+++ extensions/oembed/readme.txt
@@ -0,0 +1,17 @@
+oEmbed for PivotX
+
+Version: 0.8
+Date: 2010-05-03
+Requirements: PivotX 2.1.0 or higher
+Recommended: mysql, PHP 5.2.0 or higher
+
+Author: Two Kings // Lodewijk Evers
+E-mail: lodewijk@twokings.nl
+Support: http://forum.pivotx.net/viewtopic.php?f=10&t=1377
+Download: http://extensions.pivotx.net/entry/5/oembed
+
+Description:
+Add oEmbed tags.
+
+More documentation and installation instructions:
+See index.html in the docs folder of this module.

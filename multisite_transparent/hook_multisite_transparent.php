0099f848206ce0a907e67902f7e72846a9935b25
Hans Fredrik Nordhaug <hansfn@gmail.com>
2010-09-04 22:30:53 +0000

    Moved Multisite Transparent to extensions.

---
+++ multisite_transparent/hook_multisite_transparent.php
@@ -0,0 +1,28 @@
+<?php
+// - Extension: Multisite Transparent
+// - Version: 0.3
+// - Author: PivotX Team
+// - Email: admin@pivotx.net
+// - Site: http://www.pivotx.net
+// - Description: This hook will make the paths to resources (like images and CSS files) much cleaner for a site that is run by PivotX in multi-site mode..
+// - Date: 2010-08-24
+// - Identifier: multisite-transparent
+// - Required PivotX version: 2.0.2
+
+$this->addHook(
+    'before_output',
+    'callback',
+    'multisiteTransparentHook'
+    );
+
+
+function multisiteTransparentHook(&$html) {
+    global $PIVOTX;
+    if ($PIVOTX['multisite']->isActive()) {
+        $html = str_replace('pivotx/' . $PIVOTX['multisite']->getPath(), '', $html);
+    } 
+    return $html;
+}
+
+
+?>

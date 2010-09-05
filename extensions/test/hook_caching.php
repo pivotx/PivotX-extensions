2e2c62052acfd6a249477545b0f4358316fcb7f9
hansfn <hansfn@a2bfabd2-e71a-0410-9f27-bcdfd5ae8e08>
2010-09-05 05:49:14 +0000

    Moved Test to extensions.

---
+++ extensions/test/hook_caching.php
@@ -0,0 +1,179 @@
+<?php
+// - Extension: TEST - Example hooks for caching events
+// - Version: 0.1
+// - Author: PivotX Team
+// - Email: admin@pivotx.net
+// - Site: http://www.pivotx.net
+// - Description: Some examples to demonstrate the hooks for events that are used in caching.
+// - Date: 2009-09-15
+
+
+$this->addHook(
+    'cache_before_write',
+    'callback',
+    "cacheBeforeWrite"
+    );
+
+
+/**
+ * Example Hook Extension function for 'cache_before_write'
+ *
+ * @param string $template contains the filename of the HTML template (passed by reference)
+ */
+function cacheBeforeWrite(&$template) {
+    global $PIVOTX;
+    
+    // Do something with $template
+    
+    debug("Running 'Cache before write' hook");
+    
+    debug('The template is ' . strlen($template) . ' characters long.');
+    
+    return true;
+    
+}
+
+
+
+$this->addHook(
+    'cache_after_write',
+    'callback',
+    "cacheAfterWrite"
+    );
+
+
+/**
+ * Example Hook Extension function for 'cache_after_write'
+ *
+ * @param string $html contains the parsed HTML, as it was
+ * just saved to the cache. (passed by reference)
+ */
+function cacheAfterWrite(&$html) {
+    global $PIVOTX;
+    
+    // Do something with $html
+    
+    debug("Running 'Cache after write' hook");
+    
+    debug("The parsed HTML is: " . strlen($html) . " characters long.");
+    
+    return true;
+    
+}
+
+
+
+
+$this->addHook(
+    'cache_before_read',
+    'callback',
+    "cacheBeforeRead"
+    );
+
+
+/**
+ * Example Hook Extension function for 'cache_before_read'
+ *
+ * @param string $template contains the filename of the HTML template (passed by reference)
+ */
+function cacheBeforeRead(&$template) {
+    global $PIVOTX;
+    
+    // Do something with $template
+    
+    debug("Running 'Cache before read' hook");
+    
+    debug("The template is: " . $template);
+    
+    return true;
+    
+}
+
+
+
+
+$this->addHook(
+    'cache_after_read',
+    'callback',
+    "cacheAfterRead"
+    );
+
+
+/**
+ * Example Hook Extension function for 'cache_after_read'
+ *
+ * @param string $html contains the parsed HTML, as it is
+ * retrieved from the cache. (passed by reference)
+ */
+function cacheAfterRead(&$html) {
+    global $PIVOTX;
+    
+    // Do something with $html
+    
+    debug("Running 'Cache after read' hook");
+    
+    debug("The parsed HTML is: " . strlen($html) . " characters long.");
+    
+    return true;
+    
+}
+
+
+
+
+$this->addHook(
+    'cache_missed_read',
+    'callback',
+    "cacheMissedRead"
+    );
+
+
+/**
+ * Example Hook Extension function for 'cache_missed_read'
+ *
+ * @param string $template contains the filename of the HTML template (passed by reference)
+ */
+function cacheMissedRead(&$template) {
+    global $PIVOTX;
+    
+    // Do something with $template
+    
+    debug("Running 'Cache missed read' hook");
+    
+    debug("Apparently, we have no cached version of this page yet");
+    
+    return true;
+    
+}
+
+
+
+$this->addHook(
+    'cache_clear',
+    'callback',
+    "cacheClear"
+    );
+
+
+/**
+ * Example Hook Extension function for 'cache_clear'
+ *
+ * @param string $template contains the (partial) filename of the files that
+ * will be removed.
+ */
+function cacheClear(&$template) {
+    global $PIVOTX;
+    
+    // Do something with $template
+    
+    debug("Running 'Cache clear' hook");
+    
+    debug('The (partial) filename is ' . $template . '.');
+    
+    return true;
+    
+}
+
+
+
+?>
---
+++ extensions/test/hook_feeds.php
@@ -0,0 +1,168 @@
+<?php
+// - Extension: TEST - Example hooks for RSS and Atom feeds
+// - Version: 0.1
+// - Author: PivotX Team
+// - Email: admin@pivotx.net
+// - Site: http://www.pivotx.net
+// - Description: Some examples to demonstrate the hooks for RSS and Atom feeds.
+// - Date: 2008-10-02
+
+
+$this->addHook(
+    'feed_rss_template',
+    'callback',
+    "rssTemplateCallback"
+    );
+
+
+/**
+ * Example Hook Extension function for 'feed_rss_template'
+ *
+ * @param string $template contains the RSS template (passed by reference)
+ */
+function rssTemplateCallback(&$template) {
+    global $PIVOTX;
+    
+    // Do something with $template
+    
+}
+
+
+
+
+$this->addHook(
+    'feed_atom_template',
+    'callback',
+    "atomTemplateCallback"
+    );
+
+
+/**
+ * Example Hook Extension function for 'feed_atom_template'
+ *
+ * @param string $template contains the RSS template (passed by reference)
+ */
+function atomTemplateCallback(&$template) {
+    global $PIVOTX;
+    
+    // Do something with $template
+
+}
+
+
+
+
+$this->addHook(
+    'feed_head',
+    'callback',
+    "feedHeadCallback"
+    );
+
+/**
+ * Example Hook Extension function for 'feed_head'
+ *
+ * $replace will contain an array with all the %key% -> value pairs, used
+ * for replacing in the Feed's head section
+ *
+ * Array (
+ *   [%sitename%] => PivotX Powered
+ *   [%title%] => My weblog
+ *   [%sitename_safe%] => pivotx-powered
+ *   [%title_safe%] => my-weblog
+ *   [%link%] => http://example.org/weblog/
+ *   ..
+ * )
+ * 
+ * @param array $replace (passed by reference)
+ */
+function feedHeadCallback(&$replace) {
+    global $PIVOTX;
+    
+    // Do something with $template
+    
+    // To view all elements, use 
+    // print_r($replace);
+}
+
+
+
+$this->addHook(
+    'feed_entry',
+    'callback',
+    "feedEntryCallback"
+    );
+
+/**
+ * Example Hook Extension function for 'feed_entry'
+ *
+ * $replace will contain an array with all the %key% -> value pairs, used
+ * for replacing in each of the Feed's entry sections
+ *
+ * Array (
+ *   [%title%] => Entry title goes here
+ *   [%subtitle%] =>
+ *   [%link%] => http://example.org/archive/2008-09-29/test-3
+ *   [%description%] => <p>Lorum ipsum dolor sit amet</p>
+ * )
+ *  
+ * @param array $replace (passed by reference)
+ */
+function feedEntryCallback(&$replace) {
+    global $PIVOTX;
+    
+    // Do something with $template
+
+    // To view all elements, use 
+    // print_r($replace);
+    
+}
+
+
+
+$this->addHook(
+    'feed_comment',
+    'callback',
+    "feedCommentCallback"
+    );
+
+/**
+ * Example Hook Extension function for 'feed_comment'
+ *
+ * $replace will contain an array with all the %key% -> value pairs, used
+ * for replacing in each of the Feed's comment sections
+ * 
+ * @param array $replace (passed by reference)
+ */
+function feedCommentCallback(&$replace) {
+    global $PIVOTX;
+    
+    // Do something with $template
+
+    // To view all elements, use 
+    // print_r($replace);
+    
+}
+
+
+$this->addHook(
+    'feed_finish',
+    'callback',
+    "feedFinishCallback"
+    );
+
+
+/**
+ * Example Hook Extension function for 'feed_rss_template'. $feed contains the
+ * entire parsed feed, just before output.
+ *
+ * @param string $feed (passed by reference)
+ */
+function feedFinishCallback(&$feed) {
+    global $PIVOTX;
+    
+    // Do something with $feed
+    
+}
+
+
+?>

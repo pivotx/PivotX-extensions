a940a3a04b763200fcb9399b3edb2f71320ba7c4
Harm <1517188+Harm10@users.noreply.github.com>
2011-07-15 19:15:02 +0000

    Slideshow version 0.8

--- slideshow/docs/index.markdown
+++ slideshow/docs/index.markdown
@@ -21,11 +21,11 @@ The minimal usage is:
 
     [[ slideshow ]]
 
-The snippet takes eleven optional parameters (which override the default values):
+The snippet takes a lot of optional parameters (which override the default values):
 
   * **folder** - the name of the subfolder (of the images folder) that the slideshow should be built from.
-  * **width** - the width of (the images in) the slide show.
-  * **height** - the height of (the images in) the slide show..
+  * **width** - the width of (the images in) the slideshow.
+  * **height** - the height of (the images in) the slideshow.
   * **timeout** - the time (in milliseconds) between each image in the slideshow.
   * **limit** - the maximum number of images in the slideshow.
   * **orderby** - how the images are ordered in the slideshow. One of the
@@ -36,5 +36,12 @@ The snippet takes eleven optional parameters (which override the default values)
     be included in the slide show. One of the
     following values: no, leaf, all.
   * **nicenamewithdirs** - include directory names to the automatically generated image title.
+  * **zc** - zoom crop value to be used when creating the thumbnails (see includes/timthumb.php for details);
+    overrules the setting of timthumb_zc. Values from 0 to 3.
+  * **css** - filename of the css to use for the slideshow (defaults to slideshow).
+  * **tooltip** - when displaying the buttons (see css samples) setting tooltip to true (1) shows title
+  when hovering over button.
+  * **ttopacity** - opacity value to use for the tooltip (between 0.1 and 1.0).
+  * **uibefore** - positions the buttons before the slideshow when set to true (1).
   * **iptcindex** - index of image title in IPTC table (Picasa coments use '2#120'). 
   * **iptcencoding** - iconv encoding name of image IPTC texts.
--- slideshow/widget_slideshow.php
+++ slideshow/widget_slideshow.php
@@ -1,11 +1,11 @@
 <?php
 // - Extension: Slideshow
-// - Version: 0.7
+// - Version: 0.8
 // - Author: PivotX Team
 // - Email: admin@pivotx.net
 // - Site: http://www.pivotx.net
 // - Description: A snippet and widget to add a slideshow to your site or entries/pages/templates.
-// - Date: 2010-09-27
+// - Date: 2011-07-12
 // - Identifier: slideshow
 // - Required PivotX version: 2.0.2
 
@@ -15,6 +15,8 @@ global $slideshow_config;
 $slideshow_config = array(
     'slideshow_width' => "250",
     'slideshow_height' => "180",
+    'slideshow_zc' => "",
+    'slideshow_css' => "slideshow",
     'slideshow_folder' => "slideshow",
     'slideshow_timeout' => "4000",
     'slideshow_limit' => "15",
@@ -23,6 +25,9 @@ $slideshow_config = array(
     'slideshow_only_snippet' => 0,
     'slideshow_recursion' => 'no',
     'slideshow_nicenamewithdirs' => 0,
+    'slideshow_tooltip' => 0,
+    'slideshow_ttopacity' => "0.5",
+    'slideshow_uibefore' => 0,
     'slideshow_iptcindex' => "",
     'slideshow_iptcencoding' => "",
 );
@@ -56,17 +61,18 @@ $this->addHook(
 /**
  * Add some javascript to the header..
  */
+$css = getDefault($PIVOTX['config']->get('slideshow_css'), $slideshow_config['slideshow_css']);
 $this->addHook(
     'after_parse',
     'insert_before_close_head',
     "
     <!-- Includes for slideshow extension -->
-    <script type='text/javascript' src='[[pivotx_dir]]extensions/slideshow/jquery.slideviewer.1.1.js'></script>
-    <script type='text/javascript' src='[[pivotx_dir]]extensions/slideshow/jquery.easing.1.2.js'></script>
+    <script type='text/javascript' src='[[pivotx_dir]]extensions/slideshow/jquery.slideviewer.1.2.js'></script>
+    <script type='text/javascript' src='[[pivotx_dir]]extensions/slideshow/jquery.easing.1.3.js'></script>
     <script type='text/javascript'>
         var slideshow_pathToImage = '[[pivotx_dir]]extensions/slideshow/spinner.gif';
     </script>
-    <link href='[[pivotx_dir]]extensions/slideshow/slideshow.css' rel='stylesheet' type='text/css' />\n"
+    <link href='[[pivotx_dir]]extensions/slideshow/" . $css . ".css' rel='stylesheet' type='text/css' />\n"
 );
 
 // If the hook for the jQuery include in the header was not yet installed, do so now..
@@ -133,8 +139,8 @@ function smarty_slideshow($params) {
 
     $js_insert = <<<EOF
 <script type="text/javascript">
-jQuery(function(){
-    jQuery("div#pivotx-slideshow-%count%").slideView();
+$(window).bind("load", function(){
+    jQuery("div#pivotx-slideshow-%count%").slideView(%parms%);
     setTimeout('slideNext_%count%()', %timeout%);
 });
 
@@ -153,8 +159,8 @@ function slideNext_%count%() {
         slideNext_%count%.currentslide = 0;
     }
 
-    var xpos = -slidewidth * slideNext_%count%.currentslide;
-    jQuery("div#pivotx-slideshow-%count%").find("ul").animate({ left: xpos}, 1200, "easeInOutExpo");    
+    var xpos = (-slidewidth * slideNext_%count%.currentslide);
+    jQuery("div#pivotx-slideshow-%count%").find("ul").animate({ left: xpos}, 1200, "easeInOutExpo");  
 
     setTimeout('slideNext_%count%()', %timeout%);
 
@@ -228,10 +234,32 @@ EOF;
 
     // Cut it to the desired length..
     $images = array_slice($images, 0, $limit);
+    
+    // Built the parms
+    $tooltip = getDefault($PIVOTX['config']->get('slideshow_tooltip'), $slideshow_config['slideshow_tooltip']);
+    $ttopacity = getDefault($PIVOTX['config']->get('slideshow_ttopacity'), $slideshow_config['slideshow_ttopacity']);
+    $uibefore = getDefault($PIVOTX['config']->get('slideshow_uibefore'), $slideshow_config['slideshow_uibefore']);
+    $zc = getDefault($PIVOTX['config']->get('slideshow_zc'), $slideshow_config['slideshow_zc']);
+    $zcimg = '';
+    if (isset($zc)) {
+        $zcimg = '&amp;zc=' . $zc;
+    }
+    if ( $tooltip == 1 ) {
+        $parms = "{toolTip: true";
+    } else {
+        $parms = "{toolTip: false";
+    }
+    $parms .= ", ttOpacity: " . $ttopacity;
+    if ( $uibefore == 1) {
+        $parms .= ", uiBefore: true}";
+    } else {
+        $parms .= ", uiBefore: false}";
+    }
 
     $js_insert = str_replace('%timeout%', $timeout, $js_insert);
     $js_insert = str_replace('%count%', $slideshowcount, $js_insert);
     $js_insert = str_replace('%amount%', count($images), $js_insert);
+    $js_insert = str_replace('%parms%', $parms, $js_insert);
     $PIVOTX['extensions']->addHook('after_parse', 'insert_before_close_head', $js_insert);
 
     // If a specific popup type is selected execute the callback.
@@ -267,15 +295,14 @@ EOF;
         if (!$title) {
             $title = $nicefilename;
         }
- 
         $line = "<li>\n";
         if ($popup != 'no') {
             $line .= sprintf("<a href=\"%s%s\" class=\"$popup\" rel=\"slideshow\" title=\"%s\">\n",
                 $PIVOTX['paths']['upload_base_url'], $image, $title);
         }
-        $line .= sprintf("<img src=\"%sincludes/timthumb.php?src=%s&amp;w=%s&amp;h=%s&amp;zc=1\" " .
+        $line .= sprintf("<img src=\"%sincludes/timthumb.php?src=%s&amp;w=%s&amp;h=%s%s\" " .
                 "alt=\"%s\" width=\"%s\" height=\"%s\" />\n",
-            $PIVOTX['paths']['pivotx_url'], rawurlencode($image), $width, $height, $title, $width, $height);
+            $PIVOTX['paths']['pivotx_url'], rawurlencode($image), $width, $height, $zcimg, $title, $width, $height);
         if ($popup != 'no') {
             $line .= "</a>";
         }
@@ -311,7 +338,10 @@ function slideshowAdmin(&$form_html) {
         'label' => __('Default folder name'),
         'value' => '',
         'error' => __('That\'s not a proper folder name!'),
-        'text' => __("The name of the folder in where the images are that Slideshow should use. This should be a folder inside your <tt>images</tt> folder. So if you input <tt>slideshow</tt>, the slideshow will look in the <tt>/images/slideshow/</tt> folder. Don't start or finish with a slash."),
+        'text' => __("The name of the folder in where the images are that Slideshow should use. 
+        This should be a folder inside your <tt>images</tt> folder. 
+        So if you input <tt>slideshow</tt>, the slideshow will look in the <tt>/images/slideshow/</tt> folder. 
+        Don't start or finish with a slash."),
         'size' => 32,
         'isrequired' => 1,
         'validation' => 'string|minlen=1|maxlen=32'
@@ -347,10 +377,37 @@ function slideshowAdmin(&$form_html) {
         'label' => __('Default height'),
         'value' => '',
         'error' => __('Error!'),
-        'text' => __("The width and height of the thumbnails in the widget. The borders are added to this, so the total dimensions of the widget will be 6 pixels wider and taller."),
+        'text' => __("The width and height of the thumbnails in the widget. 
+        The borders are added to this, so the total dimensions of the widget can be wider and taller."),
         'size' => 6,
         'isrequired' => 1,
-        'validation' => 'string|min=1|max=500'
+        'validation' => 'integer|min=1|max=500'
+    ));
+    
+    $form->add( array(
+        'type' => 'text',
+        'name' => 'slideshow_zc',
+        'label' => __('Default zoom crop'),
+        'value' => '',
+        'error' => __('Error!'),
+        'text' => __("The zoom crop value to use when creating thumbnails. 
+        See includes/timthumb.php for an explanation. 
+        Also notice the potential influence of timhumb_zc setting."),
+        'size' => 3,
+        'isrequired' => 0,
+        'validation' => 'integer|min=0|max=3'
+    ));  
+    
+    $form->add( array(
+        'type' => 'text',
+        'name' => 'slideshow_css',
+        'label' => __('Default css'),
+        'value' => '',
+        'error' => __('Error!'),
+        'text' => __("The css to use for slideshow."),
+        'size' => 50,
+        'isrequired' => 1,
+        'validation' => 'string|minlen=3|maxlen=50'
     ));
 
     $form->add( array(
@@ -359,7 +416,8 @@ function slideshowAdmin(&$form_html) {
         'label' => __('Limit'),
         'value' => '',
         'error' => __('Error!'),
-        'text' => __("This limits the number of items that are shown. If you set it too high, it will take longer to load your site."),
+        'text' => __("This limits the number of items that are shown. 
+        If you set it too high, it will take longer to load your site."),
         'size' => 6,
         'isrequired' => 1,
         'validation' => 'string|min=1|max=500'
@@ -408,7 +466,8 @@ function slideshowAdmin(&$form_html) {
         'isrequired' => 1,
         'validation' => 'any',
         'text' => sprintf('<p>%s</p>', 
-            __("If recursion is enabled images from either all subdirectories or just the leaf subdirectories will be included in the slide show."))
+            __("If recursion is enabled images from either all subdirectories or just the leaf 
+            subdirectories will be included in the slide show."))
     ));
 
     $form->add( array(
@@ -424,6 +483,38 @@ function slideshowAdmin(&$form_html) {
         'label' => __("Use directories in title"),
         'text' => __("Yes, include directory names to the automatically generated image titles.")
     ));
+    
+    $form->add( array(
+        'type' => 'custom',
+        'text' => sprintf("<tr><td colspan='2'><h3>%s</h3> <em>(%s)</em></td></tr>",
+            __('Buttons'),
+            __('Warning! If you use the default css (slideshow) then the buttons are hidden!') )
+    ));
+    
+    $form->add( array(
+        'type' => 'checkbox',
+        'name' => 'slideshow_tooltip',
+        'label' => __("Show title when hovering over buttons"),
+        'text' => __("Yes, include the title as a tooltip for the buttons.")
+    ));
+    
+    $form->add( array(
+        'type' => 'text',
+        'name' => 'slideshow_ttopacity',
+        'label' => __('Tooltip opacity'),
+        'value' => '',
+        'error' => __('Error!'),
+        'text' => __("Set the opacity of the shown tooltip."),
+        'size' => 3,
+        'isrequired' => 1,
+        'validation' => 'string|min=0.1|max=1.0'
+    )); 
+    $form->add( array(
+        'type' => 'checkbox',
+        'name' => 'slideshow_uibefore',
+        'label' => __("Show buttons before slideshow"),
+        'text' => __("Yes, show the buttons first and then the slideshow.")
+    ));
 
     $form->add( array(
         'type' => 'custom',
@@ -437,7 +528,8 @@ function slideshowAdmin(&$form_html) {
         'name' => 'slideshow_iptcindex',
         'label' => __('IPTC Index'),
         'value' => '',
-        'text' => __("Index of image title in IPTC table. (Picasa comments use '2#120'). Leave blank to generate a nicename."),
+        'text' => __("Index of image title in IPTC table. (Picasa comments use '2#120'). 
+        Leave blank to generate a nicename."),
         'size' => 32,
     ));
 
@@ -446,10 +538,12 @@ function slideshowAdmin(&$form_html) {
         'name' => 'slideshow_iptcencoding',
         'label' => __('IPTC Encoding'),
         'value' => '',
-        'text' => __("Encoding of image IPTC texts. (Use the iconv encoding names.) Leave blank for no decoding."),
+        'text' => __("Encoding of image IPTC texts. (Use the iconv encoding names.) 
+        Leave blank for no decoding."),
         'size' => 32,
     ));
 
+    $form->use_javascript(true);
 
     /**
      * Add the form to our (referenced) $form_html. Make sure you use the same key

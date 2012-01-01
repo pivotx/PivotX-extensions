005c3de80ecc5e49751eb8416e163a09615223be
hansfn <hansfn@a2bfabd2-e71a-0410-9f27-bcdfd5ae8e08>
2012-01-01 17:41:04 +0000

    Replaced tabs with 4 spaces - no functional changes.

--- extensions/facebooklike/hook_facebooklike.php
+++ extensions/facebooklike/hook_facebooklike.php
@@ -18,16 +18,16 @@ function smarty_facebook_like($params, &$smarty) {
     $entry = $vars['entry'];
     $page = $vars['page'];
 
-	$host = stripTrailingSlash($PIVOTX['config']->get('canonical_host'));
+    $host = stripTrailingSlash($PIVOTX['config']->get('canonical_host'));
 
-	// Setting variables for the facebook widget..
-	$width = getDefault($params['width'], '450');
-	$height = getDefault($params['height'], '80');
-	$urlparams['layout'] = "layout=" . getDefault($params['layout'], 'standard');
-	$urlparams['show_faces'] = "show_faces=" . getDefault($params['show_faces'], 'true');
-	$urlparams['action'] = "action=" . getDefault($params['action'], 'like');
-	$urlparams['font'] = "font=" . getDefault($params['font'], 'arial');
-	$urlparams['colorscheme'] = "colorscheme=" . getDefault($params['colorscheme'], 'light');
+    // Setting variables for the facebook widget..
+    $width = getDefault($params['width'], '450');
+    $height = getDefault($params['height'], '80');
+    $urlparams['layout'] = "layout=" . getDefault($params['layout'], 'standard');
+    $urlparams['show_faces'] = "show_faces=" . getDefault($params['show_faces'], 'true');
+    $urlparams['action'] = "action=" . getDefault($params['action'], 'like');
+    $urlparams['font'] = "font=" . getDefault($params['font'], 'arial');
+    $urlparams['colorscheme'] = "colorscheme=" . getDefault($params['colorscheme'], 'light');
 
     if ((!isset($params['link'])) && (isset($params['href']))) {
         $params['link'] = $params['href'];
@@ -40,26 +40,25 @@ function smarty_facebook_like($params, &$smarty) {
         $params['link'] = smarty_link(array('uri'=>$params['uri'],'hrefonly'=>1),$PIVOTX['template']);
     }
 
-	if (isset($params['link']) && !empty($params['link'])) {
+    if (isset($params['link']) && !empty($params['link'])) {
         if (preg_match('|^https?://|',$params['link'],$match)) {
             $urlparams['href'] = "href=" . urlencode($params['link']);
-        }
-        else {
+        } else {
             $urlparams['href'] = "href=" . urlencode($host.$params['link']);
         }
-	} else if (!empty($entry['link'])) {
-		$urlparams['href'] = "href=" . urlencode($host.$entry['link']);		
-	} else {
-		$urlparams['href'] = "href=" . urlencode($host.$page['link']);
-	}
-	
-	$urlparams = implode("&amp;", $urlparams);
-	
-	$html = "<iframe src=\"http://www.facebook.com/widgets/like.php?{$urlparams}\"
+    } else if (!empty($entry['link'])) {
+        $urlparams['href'] = "href=" . urlencode($host.$entry['link']);        
+    } else {
+        $urlparams['href'] = "href=" . urlencode($host.$page['link']);
+    }
+    
+    $urlparams = implode("&amp;", $urlparams);
+    
+    $html = "<iframe src=\"http://www.facebook.com/widgets/like.php?{$urlparams}\"
         scrolling=\"no\" frameborder=\"0\"
         style=\"border:none; width:{$width}px; height:{$height}px\"></iframe>";
-		
-	return $html;
+        
+    return $html;
 
 }
     

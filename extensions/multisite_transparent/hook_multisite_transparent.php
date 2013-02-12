b46690de5f7100aef79da7383cacbd2b65b139a3
hansfn <hansfn@a2bfabd2-e71a-0410-9f27-bcdfd5ae8e08>
2013-02-12 10:35:40 +0000

    Version 0.4 which fixes an incompatibility with Minify.

--- extensions/multisite_transparent/hook_multisite_transparent.php
+++ extensions/multisite_transparent/hook_multisite_transparent.php
@@ -1,11 +1,11 @@
 <?php
 // - Extension: Multisite Transparent
-// - Version: 0.3
+// - Version: 0.4
 // - Author: PivotX Team
 // - Email: admin@pivotx.net
 // - Site: http://www.pivotx.net
 // - Description: This hook will make the paths to resources (like images and CSS files) much cleaner for a site that is run by PivotX in multi-site mode..
-// - Date: 2010-08-24
+// - Date: 2013-02-12
 // - Identifier: multisite-transparent
 // - Required PivotX version: 2.0.2
 
@@ -18,7 +18,9 @@ $this->addHook(
 
 function multisiteTransparentHook(&$html) {
     global $PIVOTX;
-    if ($PIVOTX['multisite']->isActive()) {
+    if ($PIVOTX['config']->get('minify_frontend')) {
+        debug("The Multisite Transparent hook isn't run since Minify (in the frontend) is enabled");
+    } elseif ($PIVOTX['multisite']->isActive()) {
         $html = str_replace('pivotx/' . $PIVOTX['multisite']->getPath(), '', $html);
     } 
     return $html;
